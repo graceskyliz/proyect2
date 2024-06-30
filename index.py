001ce6afc6b355087582e34abed51e0a569144c0
graceskyliz <glmpluz16@gmail.com>
2024-06-29 21:59:25 -0500

    new change

---
+++ index.py
@@ -0,0 +1,26 @@
+import cv2
+
+mi_imagen = cv2.imread("imagen_buena.pnge", cv2.IMREAD_GRAYSCALE)
+imagen_pequena = cv2.resize(mi_imagen,(8,8))
+
+#invertimos los colores
+#
+
+i = 0
+while i <= 7:
+    j = 0
+    while j <= 7:
+        imagen_pequena[i][j] = 255 - imagen_pequena[i][j]
+        j += 1
+    i += 1
+
+# aplicamos el martillo bonk ( 255 baja a 16 )
+
+i = 0
+while i <= 7:
+    j = 0
+    while j <= 7:
+        imagen_pequena[i][j] = imagen_pequena[i][j] / 255 * 16
+        j += 1
+    i += 1
+print(imagen_pequena)
